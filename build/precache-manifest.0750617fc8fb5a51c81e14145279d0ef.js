c27daa02da53339632a689ab000031f10657ec78
markpw <marcius.marko10@gmail.com>
2020-01-27 17:40:41 +0100

    edit_screen

---
+++ build/precache-manifest.0750617fc8fb5a51c81e14145279d0ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c2896a459f7efcffa7a58a040159c3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea912ba9fd4b390aa2be",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "1f92ca305de3dfae8618",
+    "url": "/static/js/2.149f7ed6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.149f7ed6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ea912ba9fd4b390aa2be",
+    "url": "/static/js/main.a9c49e29.chunk.js"
+  },
+  {
+    "revision": "a99bdc6f113e04915821",
+    "url": "/static/js/runtime-main.f25d8e84.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Homepage from './components/Homepage';
 import Login from './components/Login';
 import Registration from './components/Registration';
 import Profile from './components/Profile';
+import EditUserScren from './screen/EditUserScren';
 
 function App() {
   return (
@@ -16,7 +17,10 @@ function App() {
        <Header />
       </header>
       <Route path={"/"} exact component={Homepage}></Route>
+      
       <Route path={"/profile"} exact component={Profile}></Route>
+      <Route path={"/edituserscren"} exact component={EditUserScren}></Route>
+
       <Login />
       <Route path={"/registration"} exact component={Registration}></Route>
       </Router>
